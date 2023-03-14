be56f977a5d6c7c5f5629cf167a83df2c283c248
seven <zhangmin145440@ehousechina.com>
2023-03-14 22:56:29 +0800

    add

--- vite.config.ts
+++ vite.config.ts
@@ -25,5 +25,22 @@ export default defineConfig({
                 additionalData: `@use '@/assets/style/variables.scss' as *; @use '@/assets/style/mixin.scss' as *;`
             }
         }
+    },
+    build: {
+        chunkSizeWarningLimit: 1500,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString()
+                    }
+                },
+                chunkFileNames: chunkInfo => {
+                    const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : []
+                    const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]'
+                    return `js/${fileName}/[name].[hash].js`
+                }
+            }
+        }
     }
 })
